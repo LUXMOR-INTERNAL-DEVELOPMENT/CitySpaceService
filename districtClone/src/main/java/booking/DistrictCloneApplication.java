ee0a35be6611648efd7d38cc67f2f11ae941a341
kishoreLuxmorAI <kishoreimmanuel95@gmail.com>
2026-08-27 00:20:53 +0530

    Backend service setup

---
+++ districtClone/src/main/java/booking/DistrictCloneApplication.java
@@ -0,0 +1,13 @@
+package booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistrictCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistrictCloneApplication.class, args);
+	}
+
+}
---
+++ districtClone/src/test/java/ticketbooking/DistrictCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package ticketbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistrictCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
